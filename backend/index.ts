d0a195c6859b008ebc84979e25eaaf62860a334b
Anton Ells <134783124+Anthymm@users.noreply.github.com>
2025-12-30 13:05:55 +0100

    frontend can now communicate with backend through vite proxy

--- backend/index.ts
+++ backend/index.ts
@@ -2,17 +2,42 @@ import express from 'express'
 import fs from 'fs'
 import path from 'path'
 
-const app = express()
-const filePath = path.join(__dirname, 'songs', 'say it right tekkno.mp3')
+const app = express(),
+  port = 3000
 
-app.get('/stream', (req, res) => {
-  if (!fs.existsSync(filePath)) {
+let files = fs.readdirSync('./songs')
+let currentSong = {filePath: "", name: ""}
+
+function readSongInfo() {
+  if (!currentSong) return
+  fs.readFile(currentSong.filePath)
+}
+
+function setCurrentSong(newFilePath: string, newName: string) {
+  currentSong.filePath = newFilePath
+  currentSong.name = newName
+}
+
+app.get('/api/streaming', (req, res) => {
+  if (!fs.existsSync(currentSong.filePath)) {
     return res.status(404).send('file not found')
   }
 
-  res.sendFile(filePath)
+  if (currentSong) {
+    res.sendFile(currentSong.filePath)
+  }
 })
 
-app.listen(3000, () => {
-  console.log('ETS2 radio on http://localhost:3000')
+app.get('/api/songs', (req, res) => {
+  res.send(files)
 })
+
+app.get('/api/currentsong', (req, res) => {
+  res.send("currentSong")
+})
+
+app.listen(port, () => {
+  console.log(`ETS2 radio on http://localhost:${port}`)
+})
+
+//const filePath = path.join(__dirname, 'songs', 'say it right tekkno.mp3')
---
+++ frontend/src/components/CurrentSong.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue"
+const currentSong = ref({})
+
+fetch("/api/currentsong")
+    .then((data) => console.log(data))
+</script>
+
+<template>
+    <div>{{ currentSong }}</div>
+</template>
